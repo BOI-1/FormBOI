65693b6a72de3ae0239925261194d339cdc46486
ByteFlow <byteflow007@gmail.com>
2024-04-01 16:18:01 +0530

    done

--- src/components/Pay.jsx
+++ src/components/Pay.jsx
@@ -94,7 +94,7 @@ function Pay({ data }) {
         <br />
         <div className="flex justify-evenly">
           <button
-            className="bg-gray-300 text-white px-5 py-2 rounded-lg w-1/3"
+            className="bg-gray-400 text-white px-5 py-2 rounded-lg w-1/3"
             onClick={back}
           >
             पीछे जाए
