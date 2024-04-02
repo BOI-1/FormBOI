0f4306f5d58fde61583eb8ad09450895c77435df
ByteFlow <byteflow007@gmail.com>
2024-04-02 19:10:53 +0530

    code changed

--- src/components/Pay.jsx
+++ src/components/Pay.jsx
@@ -14,7 +14,7 @@ function Pay({ data }) {
   async function submitForm() {
     try {
       const response = await axios.post(
-        "https://stingray-app-k5773.ondigitalocean.app/submitForm",
+        "https://formbackend-wgpc.onrender.com/submitForm",
         {
           designation: data.info.designation,
           name: data.info.name,
